9dc64ca6c8e8fb13665b457d9c3efb53b027c397
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 20:29:18 +0200

    Update residual_nocenter.sh

--- scripts/ablations/residual_nocenter.sh
+++ scripts/ablations/residual_nocenter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-python scripts/train.py --variant hyp-residual-nocenter -c configs/train/hyp_residual_nocenter.yaml --epochs 100
+python scripts/train.py --variant hyp-residual-nocenter -c configs/train/hyp_residual_nocenter.yaml 
 python scripts/eval.py  --variant hyp-residual-nocenter --ckpt experiments/residual_nocenter/h_residual_nocenter.pth
