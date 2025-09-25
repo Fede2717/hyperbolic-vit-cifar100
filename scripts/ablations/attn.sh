2bd84fb062742dc8f7aec827fbfb1b0355d3b853
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 23:02:29 +0200

    Update attn.sh

--- scripts/ablations/attn.sh
+++ scripts/ablations/attn.sh
@@ -2,19 +2,19 @@
 set -e
 
 python scripts/train.py --variant hyp-attn \
-  -c configs/train/hyp_attn.yaml \
+  -c configs/train/hyp_attn_phase1.yaml \
   --epochs 10 \
   --ckpt experiments/linear/h_linear.pth \
   --non_strict_load \
-  --attn_phase attn-only \
+  --attn_phase attn-only
 
 python scripts/eval.py  --variant hyp-attn \
-  --ckpt experiments/attn/h_attn.pth
+  --ckpt experiments/attn_phase1/h_attn.pth
 
 python scripts/train.py --variant hyp-attn \
   -c configs/train/hyp_attn.yaml \
   --epochs 10 \
-  --ckpt experiments/attn/h_attn.pth \
+  --ckpt experiments/attn_phase1/h_attn.pth \
   --attn_phase full \
   --lr 1e-4
 
