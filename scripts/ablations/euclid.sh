51b460e6e747d97ae84fc3db4bf2ab06494aef1b
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 17:25:00 +0200

    Update euclid.sh

--- scripts/ablations/euclid.sh
+++ scripts/ablations/euclid.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
-python scripts/train.py --variant euclid -c configs/base.yaml
-python scripts/eval.py  --variant euclid --ckpt checkpoints/w_vit_euclidean.pth
+python scripts/train.py --variant euclid -c configs/train/euclid.yaml
+python scripts/eval.py  --variant euclid --ckpt experiments/euclid/w_euclid.pth
+
