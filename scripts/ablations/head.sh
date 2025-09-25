19b518d57c5a07ca56f5638d16e56489c5b69d04
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 17:25:12 +0200

    Update head.sh

--- scripts/ablations/head.sh
+++ scripts/ablations/head.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-python scripts/train.py --variant hyp-head -c configs/base.yaml
-python scripts/eval.py  --variant hyp-head --ckpt checkpoints/h_hyper_best.pth
+python scripts/train.py --variant hyp-head -c configs/train/hyp_head.yaml
+python scripts/eval.py  --variant hyp-head --ckpt experiments/head/h_head.pth
