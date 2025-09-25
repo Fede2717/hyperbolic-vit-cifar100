22bfdcdb0c8106d8970ee3d7ebf669f7fec0d813
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 17:25:26 +0200

    Update pos.sh

--- scripts/ablations/pos.sh
+++ scripts/ablations/pos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-python scripts/train.py --variant hyp-pos -c configs/base.yaml
-python scripts/eval.py  --variant hyp-pos --ckpt checkpoints/h_hyper_best.pth
+python scripts/train.py --variant hyp-pos -c configs/train/hyp_pos.yaml
+python scripts/eval.py  --variant hyp-pos --ckpt experiments/pos/h_pos.pth
