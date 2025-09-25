3834cde0e7b057bed4e1c885350891a795bfd357
Fede2717 <federico.forner.f@gmail.com>
2025-09-25 17:26:06 +0200

    Update linear.sh

--- scripts/ablations/linear.sh
+++ scripts/ablations/linear.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 python scripts/train.py --variant hyp-mlp \
@@ -6,8 +7,15 @@ python scripts/train.py --variant hyp-mlp \
   --epochs 50 \
   --total_epochs 100
 
+python scripts/eval.py  --variant hyp-mlp \
+  --ckpt experiments/linear/h_linear.pth
+
 python scripts/train.py --variant hyp-mlp \
   -c configs/train/hyp_linear.yaml \
   --epochs 50 \
-  --ckpt experiments/linear/h_hyper_best.pth \
+  --ckpt experiments/linear/h_linear.pth \
   --total_epochs 100 --resume_from_epoch 50
+
+python scripts/eval.py  --variant hyp-mlp \
+  --ckpt experiments/linear/h_linear.pth
+
